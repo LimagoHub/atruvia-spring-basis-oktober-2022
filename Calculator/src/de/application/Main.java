555b3ed1e92fb4dcde15ed2f6b620d49ecfd1ff0
Jo Wagner <jowagner@limago.de>
2022-10-25 15:54:05 +0200

    Initial Commit

--- Calculator/src/de/application/Main.java
+++ Calculator/src/de/application/Main.java
@@ -5,13 +5,15 @@ import de.atruvia.math.CalculatorImpl;
 import de.atruvia.math.CalculatorLogger;
 import de.atruvia.math.CalculatorSecure;
 import de.client.CalcClient;
+import de.common.LoggerProxy;
 
 
 public class Main {
     public static void main(String[] args) {
 
         Calculator calculator = new CalculatorImpl();
-        calculator = new CalculatorLogger(calculator);
+        //calculator = new CalculatorLogger(calculator);
+        calculator = (Calculator) LoggerProxy.newInstance(calculator);
         calculator = new CalculatorSecure(calculator);
         CalcClient client = new CalcClient(calculator);
         client.go();
